4227a207678ee3afe4b384daacc295ec7c538db5
regret546 <iregret546@gmail.com>
2023-11-16 21:47:29 +0800

    deploy to github

---
+++ app.js
@@ -0,0 +1,5 @@
+const container = document.querySelector("#container");
+const baseURL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+for (let i = 1; i < 10; i++) {}
