ae9a845aa625e28a33df897952d5cd8ebb00885f
regret546 <iregret546@gmail.com>
2023-11-16 22:04:03 +0800

    Initial output

--- app.js
+++ app.js
@@ -2,4 +2,13 @@ const container = document.querySelector("#container");
 const baseURL =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-for (let i = 1; i < 10; i++) {}
+for (let i = 1; i <= 151; i++) {
+  const pokemon = document.createElement("div");
+  container.appendChild(pokemon);
+  const img = document.createElement("img");
+  img.src = `${baseURL}${i}.png`;
+  pokemon.appendChild(img);
+  const pokemonNum = document.createElement("span");
+  pokemonNum.innerText = i;
+  pokemon.appendChild(pokemonNum);
+}
